8e160f3216df24d174459119e64e5c74f0c9c864
Stephen Eduardo Camilo <stvcomp@gmail.com>
2019-12-27 12:34:44 -0300

    load list

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,44 +1,47 @@
-document.addEventListener('DOMContentLoaded', function() {
-    try {
-        const app = firebase.app();
+document.addEventListener('DOMContentLoaded', function () {
+	try {
+		const app = firebase.app();
 
-        const features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+		const features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
 
-        // document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
+		// document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
 
-        const database = app.database();
-        tapa(database);
-        getContent(database);
-    } catch (e) {
-        console.error(e);
-         document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
-    }
+		const database = app.database();
+		tapa(database);
+	} catch (e) {
+		console.error(e);
+		document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+	}
 });
 
 function getContent(database) {
+	usersArr = [];
 	var starCountRef = database.ref('users');
-	starCountRef.on('value', function(snapshot) {
-		snapshot.forEach(function(childSnapshot) {
+	starCountRef.on('value', function (snapshot) {
+		snapshot.forEach(function (childSnapshot) {
 			var childKey = childSnapshot.key;
 			var childData = childSnapshot.val();
-			console.table(childSnapshot);
-			console.table(childSnapshot.key);
-			console.table(childSnapshot.val());
+			usersArr.push(childData);
 		});
 
 	});
+	return usersArr;
 }
 
 function tapa(database) {
-    var tapa = document.querySelector("#tapa");
-    tapa.onclick = function(e) {
-        e.preventDefault();
-        var name = document.querySelector("#name");
-        if (name.value) {
-            database.ref('users').push({
-                username: name.value,
-		    time: firebase.database.ServerValue.TIMESTAMP
-            });
-        }
-    };
+	var tapa = document.querySelector("#tapa"); 
+	tapa.onclick = function (e) {
+		e.preventDefault();
+		var name = document.querySelector("#name");
+		if (name.value) {
+			database.ref('users').push({
+				username: name.value,
+				time: firebase.database.ServerValue.TIMESTAMP
+			});
+		}
+	};
+	var users = getContent(database);
+	users.forEach(function(user){
+		console.log(user);
+	});
 }
