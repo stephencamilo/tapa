45b43dac2e16e2f27d62e3b42993d39bf1d17f2f
Stephen Eduardo Camilo <stvcomp@gmail.com>
2019-12-28 22:30:13 -0300

    Reset feature

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,47 +1,108 @@
-document.addEventListener('DOMContentLoaded', function () {
-	try {
-		const app = firebase.app();
+document.addEventListener('DOMContentLoaded', function() {
+    try {
+        const app = firebase.app();
 
-		const features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+        const features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
 
-		// document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
+        // document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
 
-		const database = app.database();
-		tapa(database);
-	} catch (e) {
-		console.error(e);
-		document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
-	}
+        const database = app.database();
+        tapa(database);
+        reset(database);
+        update_list(database);
+    } catch (e) {
+        console.error(e);
+        document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+    }
 });
 
-function getContent(database) {
-	usersArr = [];
-	var starCountRef = database.ref('users');
-	starCountRef.on('value', function (snapshot) {
-		snapshot.forEach(function (childSnapshot) {
-			var childKey = childSnapshot.key;
-			var childData = childSnapshot.val();
-			usersArr.push(childData);
-		});
+function getContentKeys(database) {
+    return new Promise((result) => {
+        usersKeyArr = [];
+        var userCount = database.ref('users');
+        userCount.on('value', function(snapshot) {
+            var itemsProcessed = 0;
+            snapshot.forEach(function(childSnapshot) {
+                var childKey = childSnapshot.key;
+                usersKeyArr.push(childKey);
+                itemsProcessed++;
+                if (itemsProcessed === snapshot.numChildren()) {
+                    result(usersKeyArr);
+                }
+            });
+        });
+    });
+}
 
-	});
-	return usersArr;
+function getContent(database) {
+    return new Promise((result) => {
+        usersArr = [];
+        var userCount = database.ref('users');
+        userCount.on('value', function(snapshot) {
+            var itemsProcessed = 0;
+            snapshot.forEach(function(childSnapshot) {
+                var childData = childSnapshot.val();
+                usersArr.push(childData);
+                itemsProcessed++;
+                if (itemsProcessed === snapshot.numChildren()) {
+                    result(usersArr);
+                }
+            });
+        });
+    });
 }
 
 function tapa(database) {
-	var tapa = document.querySelector("#tapa"); 
-	tapa.onclick = function (e) {
-		e.preventDefault();
-		var name = document.querySelector("#name");
-		if (name.value) {
-			database.ref('users').push({
-				username: name.value,
-				time: firebase.database.ServerValue.TIMESTAMP
-			});
-		}
-	};
-	var users = getContent(database);
-	users.forEach(function(user){
-		console.log(user);
-	});
+    let tapa = document.querySelector("#tapa");
+    tapa.onclick = (e) => {
+        e.preventDefault();
+        let name = document.querySelector("#name");
+        if (name.value) {
+            database.ref('users').push({
+                username: name.value,
+                time: firebase.database.ServerValue.TIMESTAMP
+            });
+        }
+        update_list(database);
+    };
+
+}
+
+function reset(database) {
+    let reset = document.querySelector("#reset");
+    reset.onclick = (e) => {
+        e.preventDefault();
+        getContentKeys(database).then((users) => {
+            var list = document.querySelector(".list");
+            list.innerHTML = '';
+            users.forEach((userKey) => {
+                database.ref('users/' + userKey).remove();
+            });
+        });
+    };
+
+}
+
+function update_list(database) {
+    let new_order = new Promise((result) => {
+        var user_order = [];
+        getContent(database).then((users) => {
+            let itemsGone = 0;
+            var list = document.querySelector(".list");
+            list.innerHTML = '';
+            users.forEach((user) => {
+                user_order.push(user.username);
+                var li = document.createElement("li");
+                li.appendChild(document.createTextNode(user.username));
+                list.appendChild(li);
+                itemsGone++;
+                if (itemsGone === users.length) {
+                    result(user_order);
+                }
+            });
+        });
+    });
+    new_order.then((user) => {
+        console.log(user);
+    });
 }
